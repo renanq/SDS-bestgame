82dfed287ce70eb862ad8fef89e8328d202944a2
renanq <renanq@gmail.com>
2020-09-16 17:36:53 -0300

    Header Component

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -8,7 +8,7 @@ const Header = () => {
             <Logo />
             <div className="logo-text">
                 <span className="logo-text-1">
-                    Big Game
+                    Best Game
                 </span>
                 &nbsp;
                 <span className="logo-text-2">
