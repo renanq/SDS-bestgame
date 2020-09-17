92fb80602cd95947bbb6696c510281924507ae09
renanq <renanq@gmail.com>
2020-09-16 22:39:46 -0300

    Configurations

--- front-web/src/config/chart-options.ts
+++ front-web/src/config/chart-options.ts
@@ -47,7 +47,7 @@ export const pieOptions = {
         horizontal: true,
         endingShape: "rounded",
         startingShape: "rounded",
-        barHeight: "60px",
+        barHeight: "24px",
       },
     },
     dataLabels: {
