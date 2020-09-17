1bba3910da16b0aa17c1081e9c01b0f75194d7d4
renanq <renanq@gmail.com>
2020-09-16 21:14:44 -0300

    Page Records

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,9 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { RecordsResponse } from './types';
+import { formatDate } from '../../utils/formatDate';
+import Pagination from '../../components/Pagination';
+import api from '../../services/api';
 import './styles.css';
 
 const Records = () => {
+
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+    const [activePage, setActivePerPage] = useState(0);
+    const [linesPerPage, setLinesPerPage] = useState(12);
+
+    //chamando a api para recuperar as informações
+    useEffect(() => {
+        api.get(`records?linesPerPage=${linesPerPage}&page=${activePage}`)
+            .then(response => {
+                setRecordsResponse(response.data)
+            });
+    },[activePage]);
+
+    const handlePageChange = (index: number) => {
+        setActivePerPage(index);
+    }
+
     return (
-        <h1>records</h1>
+        <div className="page-container">
+            <div className="filters-container records-actions">
+                <Link to="/charts">
+                    <button className="action-filters">
+                        Ver Gráfico
+                    </button>
+                </Link>
+            </div>
+            <table className="records-table">
+                <thead>
+                    <tr>
+                        <th>
+                            INSTANTE
+                        </th>
+                        <th>
+                            NOME
+                        </th>
+                        <th>
+                            IDADE
+                        </th>
+                        <th>
+                            PLATAFORMA
+                        </th>
+                        <th>
+                            GÊNERO
+                        </th>
+                        <th>
+                            GAME
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    { recordsResponse?.content.map(record => {
+                        return (
+                            <tr key={record.id}>
+                                <td>
+                                    {formatDate(record.moment)}
+                                </td>
+                                <td>
+                                    {record.name}
+                                </td>
+                                <td>
+                                    {record.age}
+                                </td>
+                                <td className="text-secondary">
+                                    {record.gamePlatform}
+                                </td>
+                                <td>
+                                    {record.genreName}
+                                </td>
+                                <td className="text-primary">
+                                    {record.gameTitle}
+                                </td>
+                            </tr>
+                        )
+                    }) }
+                </tbody>
+            </table>
+            <Pagination 
+                activePage={activePage}
+                goToPage={handlePageChange}
+                totalPages={recordsResponse?.totalPages}
+            />
+        </div>
     );
 };
 
